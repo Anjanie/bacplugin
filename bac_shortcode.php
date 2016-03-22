2b26d6b08fa16c026ad90991899fa777400f087f
AnmolJJohn <johnanm@sheridancollege.ca>
2016-03-22 16:12:18 -0400

    custom post type

--- bac_shortcode.php
+++ bac_shortcode.php
@@ -15,29 +15,44 @@ function ba_clothiers_enqueue_scripts(){
 }
 add_action('wp_enqueue_scripts', 'ba_clothiers_enqueue_scripts');
 
+//The custom post type with thumbnails
+    
+function content_custom_post_type() {
+        register_post_type( 'content',
+            array(
+                'labels' => array(
+                    'name' => __( 'content' ),
+                    'singular_name' => __( 'content' )
+                ),
+                'public' => true,
+                'has_archive' => true
+            )
+        );
+    }
+    add_action( 'init', 'content_custom_post_type' );
+    add_post_type_support( 'content', 'thumbnail' );
+
 //This code adds a shortcode button on the back end of wordpress.
-    add_shortcode( 'content_shortcode', 'content_custom_post_type' );
+    add_shortcode( 'content_shortcode', 'content_short_code' );
 
 //This code calls onto the shortcode function.
-    function content_custom_post_type(){
+    function content_short_code(){
         $args = array(
-            'post_type' => '', //
+            'post_type' => 'content', //
             'post_status' => 'publish',
 			'posts_per_page' => 3
         );
-
-        $string = '';
+ $string = '';
         $query = new WP_Query( $args );
         if( $query->have_posts() ){
-            $string .= '<ul>';
             while( $query->have_posts() ){
                 $query->the_post();
-                $string .= '<li>' . get_the_title() . the_post_thumbnail() . '</li>';
+                $string .= get_the_title() . the_post_thumbnail();
             }
-            $string .= '</ul>';
         }
         wp_reset_postdata();
         return $string;
+       
     }
 
 	
