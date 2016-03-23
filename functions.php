856fe33719f76e1a3fdaffcc915f94a3aefbc192
Bisma Ayyaz <bismaayyaz_19@live.com>
2016-03-22 22:52:46 -0400

    Added functions to call the widget

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/*
+	Plugin Name: BA Clothiers Widget
+	Description: This is a widget to display yearly archives.
+	Plugin URI: http://google.com
+	Author: Bisma Ayyaz, Anmol Joy, Anjanie Rupnarain
+	Author URI: http://google.com
+	License: GPL2
+	Version: 1.0
+*/
+
+
+// Call the file that makes our new widget
+require get_stylesheet_directory() .'/bacplugin/bac-widget.php';
