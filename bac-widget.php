3632c0e93e1a5206fe26cad3d7b990b3ab3d0a39
Bisma Ayyaz <bismaayyaz_19@live.com>
2016-03-22 22:36:49 -0400

    Correction of Spellings

--- bac-widget.php
+++ bac-widget.php
@@ -1,6 +1,6 @@
 <?php	
 /*
-	Plugin Name: BA Clotheirs Widget
+	Plugin Name: BA Clothiers Widget
 	Description: This is a widget to display yearly archives.
 	Plugin URI: http://google.com
 	Author: Bisma Ayyaz, Anmol Joy, Anjanie Rupnarain
